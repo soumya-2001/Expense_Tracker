6b28d74cf0958d9c1fc052d992058567d99671de
soumya <soumyamariyamoommen@gmail.com>
2024-02-15 15:30:33 +0530

    final commit

--- budget/models.py
+++ budget/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here
 class Transaction(models.Model):
@@ -19,7 +20,7 @@ class Transaction(models.Model):
     )
     category=models.CharField(max_length=200,choices=cat_options,default="miscellaneous")
     created_date=models.DateTimeField(auto_now_add=True,blank=True)
-    user=models.CharField(max_length=200)
+    user_object=models.ForeignKey(User,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
--- fundflowapplication/urls.py
+++ fundflowapplication/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from budget import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('transactions/all/',views.TransactionListView.as_view(),name="transaction-list"),
+    path('transactions/add/',views.TransactionCreateView.as_view(),name="transaction-add"),
+    path('transactions/<int:pk>/',views.TransactionDetailView.as_view(),name="transaction-detail"),
+    path('transactions/<int:pk>/remove/',views.TransactionDeleteView.as_view(),name="transaction-remove"),
+    path('transactions/<int:pk>/change/',views.TransactionUpdateView.as_view(),name="transaction-change"),
+    path('signup/',views.SignUpView.as_view(),name="signup"),
+    path('',views.SignInView.as_view(),name="signin"),
+    path('signout/',views.SignOutView.as_view(),name="signout"),
 ]
